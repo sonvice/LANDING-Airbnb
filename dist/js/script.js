3e6c6622cdebcc825df3de7e6992e0100cf0657a
wilsonelwilson <wvmc_es@hotmail.com>
2020-03-18 15:23:35 -0500

    Animación del menu hamburguersa y añadido archivo js

---
+++ dist/js/script.js
@@ -0,0 +1,8 @@
+
+let toggleBar = document.querySelector('.toggle__bar')
+ 
+ let toogle = document.getElementById('toggle-bar')
+ toogle.addEventListener('click', function(){
+    toogle.classList.toggle('active')
+ })
+
--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,15 @@ const gulp = require('gulp');
 const browsersync = require('browser-sync').create(); 
 const sass = require('gulp-sass');
 const pug = require('gulp-pug');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
   return gulp.src('dev/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }
+    ).on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(gulp.dest('dist/css'))
     .pipe(browsersync.stream());
 });
@@ -20,6 +25,10 @@ gulp.task('pug', function (){
                 .pipe(gulp.dest('./dist/'));
               
 });
+
+gulp.task('autoprefixer', function(){
+  return gulp.src()
+})
 
 gulp.task('default', function (){
   gulp.watch('dev/**/*.pug', gulp.series('pug'))
