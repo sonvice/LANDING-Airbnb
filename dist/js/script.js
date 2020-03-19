1152fe69657633bb0c5318236024a23a540c1fd9
wilsonelwilson <wvmc_es@hotmail.com>
2020-03-19 15:09:16 -0500

    Funcionamiento del btn toggle con el nav

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,10 @@
 
-let toggleBar = document.querySelector('.toggle__bar')
- 
- let toogle = document.getElementById('toggle-bar')
- toogle.addEventListener('click', function(){
-    toogle.classList.toggle('active')
- })
+// let toggleBar = document.querySelector('.toggle__bar');
+let nav = document.querySelector('.header__nav');
+
+let toogle = document.getElementById('toggle-bar')
+toogle.addEventListener('click', function(){
+   toogle.classList.toggle('active')
+   nav.classList.toggle('active')
+})
 
